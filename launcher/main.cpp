0e60a1dbb83cdd21396309bc7ccdd91826358c7e
droc101 <37421449+droc101@users.noreply.github.com>
2025-06-23 13:54:01 -0500

    initial commit

---
+++ launcher/main.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h>
+
+int main()
+{
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
+    {
+        printf("Error: SDL_Init(): %s\n", SDL_GetError());
+        return -1;
+    }
+
+    const float main_scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay());
+    constexpr SDL_WindowFlags window_flags = SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_RESIZABLE;
+    SDL_Window *window = SDL_CreateWindow("Game SDK",
+                                          static_cast<int>(350 * main_scale),
+                                          static_cast<int>(250 * main_scale),
+                                          window_flags);
+    if (window == nullptr)
+    {
+        printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+        return -1;
+    }
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    SDL_SetRenderVSync(renderer, 1);
+    if (renderer == nullptr)
+    {
+        SDL_Log("Error: SDL_CreateRenderer(): %s\n", SDL_GetError());
+        return -1;
+    }
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(window);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    if (SDL_GetSystemTheme() == SDL_SYSTEM_THEME_DARK)
+    {
+        ImGui::StyleColorsDark();
+    } else
+    {
+        ImGui::StyleColorsLight();
+    }
+
+    ImGuiStyle &style = ImGui::GetStyle();
+    style.ScaleAllSizes(main_scale);
+    style.FontScaleDpi = main_scale;
+
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
+    bool done = false;
+    while (!done)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+            if (event.type == SDL_EVENT_QUIT)
+                done = true;
+            if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window))
+                done = true;
+        }
+
+        if ((SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED) != 0)
+        {
+            SDL_Delay(10);
+            continue;
+        }
+
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        const ImGuiViewport *viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->WorkPos);
+        ImGui::SetNextWindowSize(viewport->WorkSize);
+        {
+            static int selected_tool = 0;
+            const char* items[] = { "GAME", "geditor", "Source Code" };
+
+            ImGui::Begin("Game SDK",
+                         nullptr,
+                         ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings);
+            const ImVec2 size = ImVec2(-1, ImGui::GetContentRegionAvail().y - 28);
+            ImGui::BeginListBox("##mainlb", size);
+            for (int i = 0; i < IM_ARRAYSIZE(items); i++)
+            {
+                if (ImGui::Selectable(items[i], selected_tool == i)) selected_tool = i;
+            }
+            ImGui::EndListBox();
+            ImGui::Separator();
+            if (ImGui::Button("Exit")) done = true;
+            ImGui::SameLine();
+            ImGui::Button("Launch");
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        SDL_SetRenderScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+        SDL_SetRenderDrawColorFloat(renderer, 0, 0, 0, 1);
+        SDL_RenderClear(renderer);
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
