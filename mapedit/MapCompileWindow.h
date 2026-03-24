7b682bedff5b5b5bec8d474ed332970b9e4d3ff1
droc101 <37421449+droc101@users.noreply.github.com>
2026-03-24 10:20:54 -0500

    further improvements to mapedit compile window

--- mapedit/MapCompileWindow.h
+++ mapedit/MapCompileWindow.h
@@ -16,19 +16,29 @@ class MapCompileWindow
 
         static void Show();
         static void Render();
+        static void RenderCompileOutput();
 
     private:
+        enum class LightingCompileMode: uint8_t
+        {
+            FULL_COMPILE,
+            FAST_COMPILE,
+            DONT_COMPILE
+        };
+
         static inline bool visible = false;
         static inline SDL_Process *compilerProcess = nullptr;
         static inline SDL_IOStream *compilerOutputStream = nullptr;
         static inline bool playMap = true;
         static inline bool bakeOnCpu = false;
-        static inline bool skipLighting = false;
         static inline std::string gameDir{};
         static inline std::string log{};
+        static inline bool outputVisible = false;
+        static inline LightingCompileMode lightCompileMode = LightingCompileMode::FULL_COMPILE;
 
         static void StartCompile();
         static void SaveLog(const std::string &path);
+        static void ProcessCompilerOutput();
 };
 
 
