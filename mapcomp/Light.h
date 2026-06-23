a7b00ea93a3b48934406b1dd69c0a4de63dbb8bf
NBT22 <91098724+NBT22@users.noreply.github.com>
2026-06-22 19:13:27 -0500

    Directional and spot lights

--- mapcomp/Light.h
+++ mapcomp/Light.h
@@ -11,6 +11,7 @@
 class Light // NOLINT(*-pro-type-member-init)
 {
     public:
+        /// @warning Changes to this enum must be mirrored in the shaders and in the engine
         enum class Type : uint32_t // NOLINT(*-enum-size)
         {
             POINT,
@@ -29,22 +30,25 @@ class Light // NOLINT(*-pro-type-member-init)
             const float *colorPtr = actor.params.at("color").Get<Color>(Color(-1u)).GetDataPointer();
             color = glm::vec3{colorPtr[0], colorPtr[1], colorPtr[2]};
             brightness = actor.params.at("brightness").Get<float>(1.0f);
-            constantAttenuation = actor.params.at("constant_attenuation").Get<float>(0.0f);
-            linearAttenuation = actor.params.at("linear_attenuation").Get<float>(0.0f);
-            quadraticAttenuation = actor.params.at("quadratic_attenuation").Get<float>(1.0f);
-            attenuationMultiplier = actor.params.at("attenuation_multiplier").Get<float>(2.0f);
 
-            if (actor.className == "light_point")
-            {
-                type = Type::POINT;
-            } else if (actor.className == "light_spot")
-            {
-                type = Type::SPOT;
-                brightAngle = actor.params.at("bright_angle").Get<float>(30.0f);
-                fadingAngle = actor.params.at("fading_angle").Get<float>(45.0f);
-            } else if (actor.className == "light_directional")
+            if (actor.className == "light_directional")
             {
                 type = Type::DIRECTIONAL;
+            } else
+            {
+                constantAttenuation = actor.params.at("constant_attenuation").Get<float>(0.0f);
+                linearAttenuation = actor.params.at("linear_attenuation").Get<float>(0.0f);
+                quadraticAttenuation = actor.params.at("quadratic_attenuation").Get<float>(1.0f);
+                attenuationMultiplier = actor.params.at("attenuation_multiplier").Get<float>(2.0f);
+                if (actor.className == "light_point")
+                {
+                    type = Type::POINT;
+                } else if (actor.className == "light_spot")
+                {
+                    type = Type::SPOT;
+                    brightAngle = actor.params.at("bright_angle").Get<float>(30.0f);
+                    fadingAngle = actor.params.at("fading_angle").Get<float>(45.0f);
+                }
             }
         }
         // NOLINTEND(*-pro-type-member-init)
