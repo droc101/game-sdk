e2b4b996962bf1bc7c2cc520981db66b17a5482e
droc101 <37421449+droc101@users.noreply.github.com>
2026-08-23 17:57:13 -0500

    deduplicate cmake configuration for gui apps

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -9,3 +9,16 @@ macro(disable_options)
         set(${OPTION_TO_SET} OFF CACHE BOOL "")
     endforeach ()
 endmacro()
+
+macro(configure_sdk_program target)
+    set_target_properties(${target} PROPERTIES LINKER_LANGUAGE CXX LINK_FLAGS "-Wl,-rpath='$ORIGIN'" PREFIX "" FOLDER "game__sdk__tools")
+
+    target_link_libraries(${target} PRIVATE game_sdk_shared)
+
+    add_dependencies(${target} assets copy_assets)
+
+    if (WIN32)
+        add_dependencies(${target} copydlls)
+        target_sources(${target} PRIVATE ../app.manifest ${target}.rc)
+    endif()
+endmacro()
